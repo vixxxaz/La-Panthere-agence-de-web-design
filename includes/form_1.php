ce0167b53708ef3b4682bcc2b10f979f733ea691
vixxxaz <99724809+vixxxaz@users.noreply.github.com>
2022-06-29 13:36:26 +0200

    session mentorat

--- includes/form_1.php
+++ includes/form_1.php
@@ -8,6 +8,7 @@
 	$email = $_POST['email'];
 	$input_504 = $_POST['input_504'];
 	$message = $_POST['message'];
+	$settings['block_interest_cohort'] = FALSE;
 	
 	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
 
