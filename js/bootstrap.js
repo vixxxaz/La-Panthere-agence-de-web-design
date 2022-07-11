40ec4c15bd094eb4891eaae408309d874334514e
vixxxaz <99724809+vixxxaz@users.noreply.github.com>
2022-07-11 15:40:52 +0200

    ajustement

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -4,7 +4,7 @@
  * Licensed under the MIT license
  */
 if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requires jQuery"); + function(a) {
-    "use strict";
+    // "use strict";
     var b = a.fn.jquery.split(" ")[0].split(".");
     if (b[0] < 2 && b[1] < 9 || 1 == b[0] && 9 == b[1] && b[2] < 1) throw new Error("Bootstrap's JavaScript requires jQuery version 1.9.1 or higher")
 }(jQuery), + function(a) {
